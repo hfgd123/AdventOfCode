e24d0a2266d17039c92d0bede911138ce800af88
hfgd <jan.lunz@pm..me>
2025-12-04 11:13:32 +0100

    Passau 3

--- passau-aoc/day3/ptask1.js
+++ passau-aoc/day3/ptask1.js
@@ -4,11 +4,36 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
-
+var testcaseLines = [];
+var values = [];
+var output = [];
 
 function solve(testcase) {
+    // Fill values array with all matching testcase lines
+    for (const testcaseElement of testcaseLines) {
+        if (testcaseElement.startsWith(testcase)) {
+            values.push(testcaseElement);
+        }
+    }
+    // Add count to output and sort
+    for (const value of values) {
+        if (value === null) {
+            continue;
+        }
+        let count = 0;
+        for (let i = 0; i < values.length; i++) {
 
-    return solution;
+            if (values[i] === value && values[i] !== null) {
+                count++;
+                values[i] = null; // Mark as counted
+            }
+        }
+        output.push(value + " " + count);
+        output.sort((a, b) => a.localeCompare(b));
+    }
+    for (const outputElement of output) {
+        process.stdout.write(outputElement + "\n");
+    }
 }
 
 var numTestcases = null;
@@ -18,9 +43,10 @@ rl.on("line", function(line) {
     if (numTestcases === null) {
         numTestcases = parseInt(line);
     } else if (done < numTestcases) {
-        var solution = solve(line);
-        process.stdout.write(solution + "\n");
+        testcaseLines.push(line);
+        done++;
+    } else if (done === numTestcases) {
+        solve(line);
         done++;
     }
 });
-
